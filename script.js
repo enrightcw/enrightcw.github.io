25eddac4b94fafe02cdbee12b936380d8eaef0d6
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-05-26 14:53:10 -0400

    css mostly complete, footer in a good place, starting to add profile text and working on contact section.

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 
     const bio = function(){
         $(".display").empty();
-        let divEl = $("<div>").addClass("bioDiv");
-        let h2El = $("<h2>").text("TEST PUT SOMETHING HERE");
+        let divEl = $("<div>").addClass("mainDiv");
+        divEl.addClass("border border-white row");
         let imgEl = $("<img>").addClass("bioImg");
+        let h2El = $("<h2>").text("While contending with constantly competing priorities I’ve succeeded at my job, and delivered fully realized projects.  I have a history of decreasing project costs, and increasing recurring revenue; I’ve had to do this with fewer resources, while maintaining multiple streams of work in parallel.");
+        h2El.addClass("mainText");
+     
         $(imgEl).attr("src", "./Assets/Images/Headshot.png");
         $(imgEl).attr("rel", "Chris Enright");
 
