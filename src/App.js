bcf62eefd03e38574520bf80d0105ee8a153d5ed
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-25 22:09:17 -0400

    Pivoting to a new react layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navigator from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Navigator />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'main-container'>
+      <Router>
+        <div>
+          <NavTabs />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+
+        </div>
+      </Router>
+      <footer className='footer mt-auto py-3 bg-dark text-white'>
+          <div className='container'>Place sticky footer content here.</div>
+      </footer>
     </div>
   );
 }
