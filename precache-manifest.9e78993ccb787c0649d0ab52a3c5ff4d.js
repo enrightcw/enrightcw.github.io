59dfd4b61b9dd17c0b22f1e7295a447452fd07c0
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-27 21:48:35 -0400

    Prepped for guild on GitHub

---
+++ precache-manifest.9e78993ccb787c0649d0ab52a3c5ff4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd06248bf0b6071befbd128b27a89fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddca8d7bc2a2180ee9e2",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "569f5e5c1b5a65da593d",
+    "url": "/static/css/main.cd6ea3c0.chunk.css"
+  },
+  {
+    "revision": "ddca8d7bc2a2180ee9e2",
+    "url": "/static/js/2.1c7128e1.chunk.js"
+  },
+  {
+    "revision": "569f5e5c1b5a65da593d",
+    "url": "/static/js/main.146f5d9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ae70b87734424573e32c473c771609b",
+    "url": "/static/media/burger.8ae70b87.png"
+  },
+  {
+    "revision": "4a9a21c12ef7b935892b9ad4d6deb4dc",
+    "url": "/static/media/noteTaker.4a9a21c1.png"
+  },
+  {
+    "revision": "38d0f1b856799c3ad4c066f19c66d2f4",
+    "url": "/static/media/scf.38d0f1b8.png"
+  },
+  {
+    "revision": "ad3f320cf911e89867161c59ae1d5a4f",
+    "url": "/static/media/soon.ad3f320c.png"
+  }
+]);
